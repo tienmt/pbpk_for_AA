56b881aa8673e85c39be301b92b2036822075381
The Tien Mai <39219912+tienmt@users.noreply.github.com>
2025-04-30 10:04:30 +0200

    Update with protein bidding, turn over
    in Kidney and Liver

--- pbpk_model_separate_AA_GA_.R
+++ pbpk_model_separate_AA_GA_.R
@@ -5,7 +5,6 @@ graphics.off()
 
 #body weight in kg
 BW = 70 
-
 # fractions of BW
 ##https://journals.sagepub.com/doi/pdf/10.1177/ANIB_32_3-4
 F_B = 0.079 
@@ -14,20 +13,17 @@ F_B_VB = 0.65 #fraction: venous blood of blood volume
 F_Li = 0.026
 F_Ki = 0.0044
 F_T = 1-(F_B+F_Li+F_Ki)
-
 #organ volumes in L
 V_AB <- BW*F_B*F_B_AB 
 V_VB <- BW*F_B*F_B_VB 
 V_T <- BW*F_T
 V_Ki <- BW*F_Ki
 V_Li <- BW*F_Li
-
 #cardiac output in L/(kg*h)
 ##https://journals.sagepub.com/doi/pdf/10.1177/ANIB_32_3-4
 QCC = 16        #12.5 # maybe 14 or 6.5 check?
 Q_C = QCC*BW^0.75
 FQ_P = 2.5/100 
-
 FQ_Li = 0.19
 FQ_Ki = 0.255 #total - there is also an aterial flow rate 
 FQ_T = 1 -( FQ_Li + FQ_Ki +FQ_P)
@@ -56,8 +52,6 @@ k_syn_GSH = 0.25 #mmol/h
 k_cl_GSH = 0.35 #1/h
 k_onAA_GSH = 0.55 #L/(mmol h)
 k_onGA_GSH = 0.8 #L/(mmol h)
-
-
 
 k_onGA_T = 0.089 #1/h
 k_onGA_B = 0.108 #1/h
@@ -96,10 +90,12 @@ KMGG = 0.1        # !KM with respect to GSH for AA or GA conjugation with GSH mM
 KMG2 = 100/MW_ga  #!Km with respect to GA for GSH conjugation mM
 
 
-kpbl1 = 0.1      # !protein binding in liver
-reactratio = 2.0 # !reactivity ratio with protein, AA/GA
-kpbl2 = kpbl1/reactratio    # !protein binding in liver
-KPTL = 0.015     # !'protein turnover rate in liver'
+KPT_Li = 0.015     # !'protein turnover rate in liver'
+KPT_Ki = 0.013     #  !'protein turnover rate in kidney'
+KPTR = 0.013     # !'protein turnover rate in rpt'
+KPTS = 0.0039    # !'protein turnover rate in spt'
+KPTRB = 0.0039   # !'protein turnover rate in rbc'
+KPTPL = 0.0039   # !'protein turnover rate in plasma'
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # create list of parameter
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -115,7 +111,7 @@ yini <- c(m_AA_AB = 0.0, m_GA_AB = 0.0, m_AA_VB = 0.0, m_GA_VB = 0.0,
           m_AA_Ki = 0.0, m_GA_Ki = 0.0, m_AA_dose = 0 ,
           m_AA_Li = 0.0, m_AAMA = 0.0,  #m_AAMA_urinary = 0.0,
           m_GA_Li = 0.0,
-          apbL2   = 0.0,
+          a_pb_GA_Li   = 0.0,
           m_GAMA  = 0.0,
           #m_GAMA_urinary = 0.0, 
           m_GSH_Li = k_0_GSH * V_Li * MW_GSH, 
@@ -128,13 +124,17 @@ yini <- c(m_AA_AB = 0.0, m_GA_AB = 0.0, m_AA_VB = 0.0, m_GA_VB = 0.0,
           m_GA_out = 0.0,
           m_GA_accum = 0.0,
           m_GA_free = 0.0,
-          m_GA_in = 0.0
+          m_GA_in = 0.0,
+          a_pb_AA_Ki = 0,
+          a_pb_AA_Li = 0,
+          a_pb_GA_Ki = 0
 )
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # PBPK model for Acrylamide 
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 PBPKmodelAA <- function(t,state,parameter){
   with(as.list(c(t,state,parameter)), {
+    ########################################################################################  
     #----#---#  Model for aa  #---#---#
     #concentrations blood in the organs in mg/L
     c_AA_AB <- m_AA_AB/V_AB ;    c_AA_VB <- m_AA_VB/V_VB
@@ -150,16 +150,25 @@ PBPKmodelAA <- function(t,state,parameter){
     dm_AA_VB <- Q_T*(c_AA_T/pAA_TB) +Q_Li*(c_AA_Li/pAA_LiB) +Q_Ki*(c_AA_Ki/pAA_KiB) - Q_C*c_AA_VB -k_onAA_B*m_AA_VB
     # units checked -> mg/h   
     dm_AA_Ki <- Q_Ki*c_AA_AB -Q_Ki*(c_AA_Ki/pAA_KiB) -k_onAA_Ki*m_AA_Ki 
+    
+    # protein tunr over AA in Kidney
+    da_pb_AA_Ki <- k_onAA_Ki*m_AA_Ki - a_pb_AA_Ki*KPT_Ki
+    
     # units checked -> mg/h   
     dm_AA_dose <- -k_AAuptake*m_AA_dose
     # units checked -> mg/h
-    dm_GSH_Li <- AGSH0 -k_cl_GSH*m_GSH_Li 
+    dm_GSH_Li <- AGSH0 - k_cl_GSH*m_GSH_Li 
     
     metAA_GSH <- VMAXG1 *c_AA_Li * c_GSH_Li /(c_AA_Li + KMG1)/(c_GSH_Li + KMGG)
     metAA_P450 <- V_max_p450 * MW_aa*c_AA_Li/ (KM_p450+c_AA_Li)
     
     # units checked -> mg/h  
     dm_AA_Li <- Q_Li*(c_AA_AB - c_AA_Li/pAA_LiB) + k_AAuptake*m_AA_dose - k_onAA_Li*m_AA_Li  - metAA_P450 - metAA_GSH 
+    
+    
+    # protein tunr over AA in Liver
+    da_pb_AA_Li <- k_onAA_Li*m_AA_Li - a_pb_AA_Li * KPT_Li
+    
 
     # units checked -> mg/h   
     dm_AA_T <- Q_T*(c_AA_AB - c_AA_T/pAA_TB) -k_onAA_T*m_AA_T
@@ -168,11 +177,12 @@ PBPKmodelAA <- function(t,state,parameter){
     dm_AAMA <- metAA_P450 + metAA_GSH  - m_AAMA*k_exc_AAMA
     
     dm_AA_out <- metAA_P450 + metAA_GSH
+    dm_AA_in <- k_AAuptake*m_AA_dose
     dm_AA_accum <- k_onAA_B*m_AA_AB +k_onAA_B*m_AA_VB +k_onAA_T*m_AA_T +k_onAA_Li*m_AA_Li +k_onAA_Ki*m_AA_Ki
     dm_AA_free <- dm_AA_AB + dm_AA_VB +dm_AA_T +dm_AA_Li +dm_AA_Ki
-    dm_AA_in <- k_AAuptake*m_AA_dose
+########################################################################################  
 ########################################################################################    
-    #----#---#  Model for aa  #---#---#    
+    #----#---#  Model for Ga  #---#---#    
     #concentrations blood in the organs in mg/L
     c_GA_AB <- m_GA_AB/V_AB ;   c_GA_VB <- m_GA_VB/V_VB
     c_GA_T <- m_GA_T/V_T
@@ -185,12 +195,22 @@ PBPKmodelAA <- function(t,state,parameter){
     # units checked -> mg/h   
     dm_GA_Ki <- Q_Ki*c_GA_AB -Q_Ki*(c_GA_Ki/pGA_KiB) -k_onGA_Ki*m_GA_Ki 
     
+    
+    # protein tunr over AA in Kidney
+    da_pb_GA_Ki <- k_onGA_Ki*m_GA_Ki - a_pb_GA_Ki*KPT_Ki
+    
+    
     # units checked -> mg/h
     metGA_GSH <- k_onGA_GSH * c_GSH_Li *m_GA_Li / (c_GA_Li + KMG2)/(c_GSH_Li + KMGG)
     metGA_EH <- V_max_EH *MW_ga *c_GA_Li / (KM_EH + c_GA_Li)
     # units checked -> mg/h 
     dm_GA_Li <- Q_Li*(c_GA_AB - c_GA_Li/pGA_LiB) - k_onGA_Li*m_GA_Li + metAA_P450 - metGA_GSH -metGA_EH 
-    dapbL2 = kpbl2*m_GA_Li - apbL2*KPTL 
+    
+    
+    # protein tunr over GA in Liver
+    da_pb_GA_Li = k_onGA_Li*m_GA_Li - a_pb_GA_Li*KPT_Li 
+    
+    
     # units checked -> mg/h, 
     dm_GAMA <-  metGA_GSH  -m_GAMA*k_exc_GAMA
     
@@ -199,25 +219,29 @@ PBPKmodelAA <- function(t,state,parameter){
     dm_GAOH <- metGA_EH -m_GAOH*k_exc_GAMA
     
     dm_GA_out <- metGA_GSH + metGA_EH 
+    dm_GA_in <- metGA_GSH +metGA_EH
     dm_GA_accum <- k_onGA_B*m_GA_AB +k_onGA_B*m_GA_VB +k_onGA_T*m_GA_T +k_onGA_Li*m_GA_Li +k_onGA_Ki*m_GA_Ki
     dm_GA_free <- dm_GA_AB + dm_GA_VB +dm_GA_T +dm_GA_Li +dm_GA_Ki 
-    dm_GA_in <- metGA_GSH +metGA_EH
+
     
     return(list(c(dm_AA_AB,  dm_GA_AB,  dm_AA_VB,    dm_GA_VB,
                   dm_AA_Ki,  dm_GA_Ki,  dm_AA_dose,
                   dm_AA_Li,
                   dm_AAMA,   
-                  dm_GA_Li, dapbL2, dm_GAMA,
+                  dm_GA_Li, da_pb_GA_Li, dm_GAMA,
                   dm_GSH_Li, dm_AA_T,   dm_GA_T,
                   dm_GAOH,
+                  dm_AA_in,
                   dm_AA_out,
                   dm_AA_accum,
                   dm_AA_free,
-                  dm_AA_in,
                   dm_GA_out,
                   dm_GA_accum,
                   dm_GA_free,
-                  dm_GA_in
+                  dm_GA_in,
+                  da_pb_AA_Ki,
+                  da_pb_AA_Li,
+                  da_pb_GA_Ki
     )))
   })
 }
