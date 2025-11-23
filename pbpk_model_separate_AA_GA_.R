ab38bf985d5726461816a93228f80ab34ef2cd8a
Maria Kalyva <makatrik@gmail.com>
2025-11-23 17:34:34 +0100

    Changes in the eqs. of the Blood and protein binding eqs.

--- pbpk_model_separate_AA_GA_.R
+++ pbpk_model_separate_AA_GA_.R
@@ -179,23 +179,30 @@ PBPKmodelAA <- function(t, state, parameter) {
     # Blood
     # units checked -> mg/h
     dm_AA_AB <- Q_C * (c_AA_VB - c_AA_AB) - k_onAA_B * m_AA_AB
+    # suggest to change from - k_onAA_B * m_AA_AB to - k_onAA_B * c_AA_AB * V_AB
     dm_AA_VB <- Q_T * (c_AA_T / pAA_TB) + Q_Li * (c_AA_Li / pAA_LiB) + Q_Ki * (c_AA_Ki / pAA_KiB) -
       Q_C * c_AA_VB - k_onAA_B * m_AA_VB
+    # similar change from suggest to change from - k_onAA_B * m_AA_VB to - k_onAA_B * c_AA_VB * V_VB
     # overall turnover / bound pool in blood (diagnostic of binding turnover)
     da_pb_AA_B <- k_onAA_B * c_AA_AB - a_pb_AA_B * KPTRB + Q_C * (c_AA_VB - c_AA_AB)
     
-    # I think this eqs. is not correct, c_AA_AB is only arterial AA concentration not of the whole blood,
-    #Also the units are wrong of the first term, you need to multiply by V, and scale it to the
-    #total blood volume
-    #I think the last Q_C representing free chemical transport, not irreversible bound adducts, 
-    # is not needed in the more simplistic and for the whole blood.
-    #I think this would be more accurate: da_pb_AA_B <- k_onAA_B * (c_AA_AB + c_AA_VB)/2 * V_BC - a_pb_AA_B * KPTRB
-    #total blood cell volume (V_BC)
-    #V_BC <- (V_AB + V_VB) * F_BC # 
+
+    # I suggest we write that
+    #V_RBC_AB <- V_AB * F_BC
+    #V_RBC_VB <- V_VB * F_BC
     #F_BC <- 0.44  # Fraction of blood volume that is RBCs, Hematocrit
     
+    # RBC volumes in arterial and venous blood
+    #c_pb_AA_AB <- a_pb_AA_AB / V_RBC_AB  
+    #c_pb_AA_VB <- a_pb_AA_VB / V_RBC_VB  
     
-
+    # Arterial RBC binding, turn over and exchange
+    #da_pb_AA_AB <- kpbrb1 * c_AA_AB * V_RBC_AB - a_pb_AA_AB * KPTRB + Q_C * (c_pb_AA_VB - c_pb_AA_AB)
+    
+    # Venous RBC binding 
+    #da_pb_AA_VB <- kpbrb1 * c_AA_VB * V_RBC_VB - a_pb_AA_VB * KPTRB + Q_C * (c_pb_AA_AB - c_pb_AA_VB)
+    
+    
     # !terminal valine hemoglobin adducts
     dm_AA_Hb <- K_FORM_AA_VAL * c_AA_VB - K_REM_AA_VAL * m_AA_Hb
     
